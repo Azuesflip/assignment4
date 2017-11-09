d8476f4b1fd9277c28b260029760b3a5fc6f69fb
azuesflip <azuesflip@gmail.com>
2017-11-08 23:22:59 -0700

    so got some things working right but holy shit this was a shit show. I know this is not informative but.... yeah.

---
+++ api/controllers/EmployeeController.js
@@ -0,0 +1,87 @@
+/**
+ * EmployeeController
+ *
+ * @description :: Server-side logic for managing employees
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	list:function(req, res){
+        Employee.find({}).exec(function(err, employee){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.view('employee/list', {employees:employee});
+        });
+    },
+    add: function(req, res){
+        Role.find({}).exec(function(err, role) {
+            if (err) {return res.serverError(err);}
+            res.view({roles:role});
+        });
+    },
+    create:function(req, res){
+        
+        var employeename = req.body.employeename;
+        console.log(employeename);
+        var roles = req.body.selectpicker;
+        console.log(roles);
+
+        Role.findOne({name:roles}).exec(function (err, role){
+            if (err) {
+                return res.serverError(err);
+            }
+            if (!role) {
+                return res.notFound('Could not find role, sorry.');
+            }
+
+            console.log('Found "%s"', role.id);
+            Employee.create({employeename:employeename}).exec(function(err, newEmployee){
+                if(err){
+                }
+                console.log(role);
+                newEmployee.roles.add(role.id);
+                newEmployee.save();
+                
+            });
+
+        });
+        res.redirect('/employee/list');
+    },
+    delete: function(req, res){
+        Employee.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                console.log(err);
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/employee/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Employee.findOne({id:req.params.id}).exec(function(err, employee){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.view('employee/edit', {employee:employee});
+        });
+    },
+    update: function(req, res){
+        var employeename = req.body.employeename;
+        var roles = req.body.roles;
+
+        Employee.update({id: req.params.id},{employeename:employeename, roles:roles}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/employee/list');
+        });
+
+        return false;
+	}
+};
+
---
+++ api/controllers/RoleController.js
@@ -0,0 +1,67 @@
+/**
+ * RoleController
+ *
+ * @description :: Server-side logic for managing roles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	list:function(req, res){
+        Role.find({}).exec(function(err, role){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.view('role/list', {roles:role});
+        });
+    },
+    add: function(req, res){
+        res.view('role/add');
+    },
+    create:function(req, res){
+        var name = req.body.name;
+        var employeename = req.body.employeename;
+
+        Role.create({name:name, employeename:employeename}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/role/list');
+        });
+    },
+    delete: function(req, res){
+        Role.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/role/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Role.findOne({id:req.params.id}).exec(function(err, role){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.view('role/edit', {role:role});
+        });
+    },
+    update: function(req, res){
+        var name = req.body.name;
+        var employeename = req.body.employeename;
+
+        Role.update({id: req.params.id},{employeename:employeename, roles:roles}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/role/list');
+        });
+
+        return false;
+	}
+};
+
